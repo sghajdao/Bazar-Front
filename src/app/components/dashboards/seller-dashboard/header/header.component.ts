b33f2018cdcc83696fe8e18c931516fbf58a321b
unknown <saadghajdaoui@gmail.com>
2024-11-16 00:25:33 +0100

    google oauth2 fixed

--- src/app/components/dashboards/seller-dashboard/header/header.component.ts
+++ src/app/components/dashboards/seller-dashboard/header/header.component.ts
@@ -13,14 +13,14 @@ export class HeaderComponent implements OnChanges {
   @Input() response?: StoreResponse
 
   earning: number = 0
-  buying: number = 0
+  sales: number = 0
   followers: number = 0
   stars: number = 0
 
   ngOnChanges(changes: SimpleChanges): void {
     if (this.response) {
       this.response.store.sales?.forEach(product => this.earning += product.price)
-      this.buying = this.response.store.sales? this.response.store.sales.length : 0
+      this.sales = this.response.store.sales? this.response.store.sales.length : 0
       this.followers = this.response.store.followers? this.response.store.followers.length : 0
       this.stars = this.response.store.stars? this.response.store.stars.length : 0
     }
